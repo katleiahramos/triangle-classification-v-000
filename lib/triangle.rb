e5703544e08c92d985489cb91710d261473b5593
Katleiah Ramos <katleiahramos@gmail.com>
2018-04-06 18:05:11 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,10 +2,11 @@ class Triangle
   attr_accessor :type,:side1, :side2, :side3
 
   def initialize(side1:, side2:, side3:)
+    binding.pry
     @side1 = side1
     @side2 = side2
     @side3 = side3
-    @sides = [@side1, @side2, @side3].sort 
+    @sides = [@side1, @side2, @side3].sort
   end
 
   def categorize
