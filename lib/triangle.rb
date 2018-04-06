dbdd9ed52c58ad7ca162d8d7598fe47b30dc1aa2
Katleiah Ramos <katleiahramos@gmail.com>
2018-04-06 17:51:41 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,9 @@ class Triangle
   attr_accessor :type,:side1, :side2, :side3
 
   def initialization(:side1, :side2, :side3)
-
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
   end
 
   def kind
