e489fedc8cb4b087439aa51744f2460c7828c7b7
Katleiah Ramos <katleiahramos@gmail.com>
2018-04-06 18:13:37 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,7 +4,6 @@ class Triangle
   attr_accessor :type, :side1, :side2, :side3
 
   def initialize(s1, s2, s3)
-
     @side1 = s1
     @side2 = s2
     @side3 = s3
@@ -12,12 +11,11 @@ class Triangle
   end
 
   def categorize
-    if side1
-    end
+
   end
+
   def self.kind
     binding.pry
-    @type
   end
 
   class TriangleError < StandardError
