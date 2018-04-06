aac5aba37231f7af33b518058de2f6611dac2011
Katleiah Ramos <katleiahramos@gmail.com>
2018-04-06 18:04:16 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,14 +5,19 @@ class Triangle
     @side1 = side1
     @side2 = side2
     @side3 = side3
+    sides = 
   end
 
+  def categorize
+    if side1
+
+  end 
   def kind
     @type
   end
 
   class TriangleError < StandardError
-  # triangle error code
+    "This triangle is not vaid!"
   end
 
 
