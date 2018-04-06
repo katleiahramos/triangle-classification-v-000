2b358ba90df429d286d5176e2295e9c3b80561b8
Katleiah Ramos <katleiahramos@gmail.com>
2018-04-06 17:50:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,17 @@
 class Triangle
-  # write code here
+  attr_accessor :type 
+
+  def initialization(:L1, :L2, :L3)
+    
+  end
+
+  def kind 
+    @type 
+  end  
+
+  class TriangleError < StandardError
+  # triangle error code
+  end
+
+  
 end
